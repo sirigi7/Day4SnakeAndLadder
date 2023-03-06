867da1eecc8f7d81c6ee02405093379afb0b0526
sandeep <sirigi.reddy7@gmail.com>
2023-03-06 19:32:31 +0530

    Master branch

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+public class SnakeAndLadder {
+
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to snake and ladder game.");
+            int player1Position = 0; // For player 1 initial position has declared.
+        }
+    }
+
