1a5a7c343a58dddab5596dd73101588f77c32df0
sandeep <sirigi.reddy7@gmail.com>
2023-03-09 15:51:16 +0530

    Added:UC2_RollTheDice

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,9 +1,13 @@
 public class SnakeAndLadder {
 
-        public static void main(String[] args) {
 
-            System.out.println("Welcome to snake and ladder game.");
-            int player1Position = 0; // For player 1 initial position has declared.
-        }
+    static int diceRoll = 0;
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to snake and ladder game.");
+        diceRoll = ((int) (Math.random()*10)%6)+1; // Used random function to generate random numbers.
+        System.out.println(diceRoll);
+        int player1Position = 0; // For player 1 initial position has declared.
+    }
     }
 
